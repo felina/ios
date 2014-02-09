48c0cbe49f2856e9ee15ff945ed7d1dc89acd33f
sambodanis <sam.bodanis@gmail.com>
2014-02-09 18:31:03 +0000

    Loading all images crashes, trying to use uipicker

---
+++ Felina/Felina/FEPhotosCollectionViewController.h
@@ -0,0 +1,16 @@
+//
+//  FEPhotosCollectionViewController.h
+//  Felina
+//
+//  Created by Sam Bodanis on 09/02/2014.
+//  Copyright (c) 2014 Sam Bodanis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#include <AssetsLibrary/AssetsLibrary.h> 
+
+@interface FEPhotosCollectionViewController : UICollectionViewController <UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+
+
+
+@end
